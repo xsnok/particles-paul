b8cfc05eb6de9ef471fd7d8bf929cabd9d7662cb
PaulBoyko1 <Paulboyko20@gmail.com>
2025-12-02 13:11:03 -0800

    Initialize window and run particle unit tests
    Added window creation and particle unit tests to the engine.

--- Engine.cpp
+++ Engine.cpp
@@ -1,13 +1,38 @@
 #include "Engine.h"
 
 Engine::Engine() {
-
+  VideoMode comp = VideoMode::getDesktopMode();
+  m_window.create(desktop, "Click for particles!");
 }
 void Engine::run() {
+  Clock clock;
+  Time dt;
+  float sec;
 
+  cout << "Starting Particle unit tests..." << endl;
+  Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
+  p.unitTests();
+  cout << "Unit tests complete.  Starting engine..." << endl;
+  while (m_window.isOpen()) {
+    dt = clock.restart();
+    sec = dt.asSeconds();
+    input();
+    update(sec);
+    draw();
+  }
 }
 void Engine::input() {
+  Event event;
+  if (event,type == Evebt::KeyPressed) {
+    if (event.key.code == Keyboard::Escape) {
+      m_window.close();
+    }
+  }
+  if (event.type == Event::MouseButtonPressed) {
+    if (event.mouseButton.button == Mouse::Left) {
 
+    }
+  }
 }
 void Engine::update(float dtAsSeconds) {
 

9b0b440d78eb367c551f1aaae0bf2ff72d38ff1f
PaulBoyko1 <Paulboyko20@gmail.com>
2025-12-08 22:17:39 -0800

    Refactor Particle class implementation
    Refactor Particle class methods for clarity and functionality.

--- Particle.cpp
+++ Particle.cpp
@@ -4,7 +4,7 @@ Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosit
 	m_ttl = TTL;
 
 	m_numPoints = numPoints;
-	m_radiansPerSec = (float)rand() / RAND_MAX * PI;
+	m_radiansPerSec = (float)rand() / RAND_MAX * M_PI;
 	setCenter(0,0);
 	setSize(target.getSize().x, (-1.0) * target.getSize().y);
 	m_centerCoordinate = target.mapPixelToCoords(mouseClickPosition, m_cartesianPlane);
@@ -12,14 +12,15 @@ Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosit
 	m_vy = (rand() % 401) + 100;
 	if (rand() % 2 != 0) m_vx *= -1;
 	m_color1 = Color::White;
-	size_t RGD[3];
-	for (int i = 0; i < 3; i++) RGD[i] = rand() % 256;
-	m_color2 = Color(RGD[0], RGD[1], RGD[2]);
-	theta = m_radiansPerSec / 2;
-	dTheta = 2 * PI / (numPoints - 1);
+	Uint8 RGB[3];
+	for (int i = 0; i < 3; i++) RGB[i] = rand() % 256;
+	m_color2 = Color(RGB[0], RGB[1], RGB[2]);
+	float theta = ((float)rand() / RAND_MAX) * (M_PI / 2.0);
+	float dTheta = 2 * M_PI / (numPoints - 1);
 	// from j to numPoints?
-	for (int i = j; i < numPoints; i++) {
-		int r, dx, dy;
+	for (int j = 0; j < numPoints; j++) {
+		float dx, dy;
+		int r;
 		r = (rand() % 61) + 20;
 		dx = r * cos(theta);
 		dy = r * sin(theta);
@@ -29,13 +30,51 @@ Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosit
 	}
 }
 void Particle::draw(RenderTarget& target, RenderStates states) const {
-	VertexArray lines(TriangleFan, numPoints + 1);
+	VertexArray lines(TriangleFan, m_numPoints + 1);
 	Vector2f center(target.mapCoordsToPixel(m_centerCoordinate, m_cartesianPlane));
 
 	lines[0].position = center;
-	lines[0].color = m_color;
+	lines[0].color = m_color1;
+
+	for (int j = 1; j <= m_numPoints; j++) {
+		lines[j].position = (target.mapCoordsToPixel(Vector2f(m_A(0, j - 1), m_A(1, j - 1)), m_cartesianPlane));
+		lines[j].color = m_color2;
+	}
+	target.draw(lines);
 	
 }
+void Particle::update(float dt) {
+	m_ttl -= dt;
+	//the functions rotate, scale, and translate will be defined later
+	rotate(dt * m_radiansPerSec);
+	scale(SCALE);
+	float dx, dy;
+	dx = m_vx * dt;
+	m_vy -= G * dt;
+	dy = m_vy * dt;
+	translate(dx, dy);
+}
+void Particle::translate(double xShift, double yShift) {
+	TranslationMatrix T(xShift, yShift, m_numPoints);
+	m_A = m_A + T;
+	m_centerCoordinate.x += xShift;
+	m_centerCoordinate.y += yShift;
+}
+void Particle::rotate(double theta) {
+	Vector2f temp(m_centerCoordinate);
+	translate(-m_centerCoordinate.x, -m_centerCoordinate.y);
+	RotationMatrix R(theta);
+	m_A = R * m_A;
+	translate(temp.x, temp.y);
+}
+void Particle::scale(double c) {
+	Vector2f temp(m_centerCoordinate);
+	translate(-m_centerCoordinate.x, -m_centerCoordinate.y);
+	ScalingMatrix S(c);
+	m_A = S * m_A;
+	translate(temp.x, temp.y);
+}
+
 bool Particle::almostEqual(double a, double b, double eps)
 {
 	return fabs(a - b) < eps;
@@ -179,6 +218,7 @@ void Particle::unitTests()
 
     cout << "Score: " << score << " / 7" << endl;
 }
+
 
 
 
